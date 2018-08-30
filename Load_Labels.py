e1ef2f45cfe3906a0e6e7710317d3ed328c13018
AxelAgue <axel.aguerreberry@gmail.com>
2018-08-30 11:59:44 -0300

    nuevos labels

--- Load_Labels.py
+++ Load_Labels.py
@@ -1,9 +1,17 @@
 import xml.etree.ElementTree as ET
 import numpy as np
 
-num_image = "004"
+"""
+PATHS:
 
-tree = ET.parse("C:/Users/axel_/Desktop/Varios/BCCD_Dataset/BCCD/Annotations/BloodImage_00" + num_image + ".xml")
+OMEN: "C:/Users/axel_/Desktop/Varios/BCCD_Dataset/BCCD/Annotations/BloodImage_00" + path + ".xml"
+SAMIN: "/home/oem/BCCD_Dataset/BCCD/Annotations/BloodImage_00" + path + ".xml"
+GUAMAN:
+
+"""
+
+# num_image = 1
+
 
 def give_label(tree):
 	labels = []
@@ -97,7 +105,6 @@ def give_label(tree):
 			labels.append(aux)
 	return np.asarray(labels)
 
-
 def labels_total(label):
 	n_grid_cells = 10
 
@@ -136,40 +143,166 @@ def labels_total(label):
 			else:
 				l_total.append(b)
 
-	return l_total
 
+
+
+
+	return np.asarray(l_total)
+
+def correct_label(label):
+
+	lenght = label.shape[0]
+	total = []
+	for i in range(lenght):
+		alpha = label[i]
+
+		aa = label[i]
+
+		if aa[1] < 80:
+			alpha[1] = 0
+		else:
+			alpha[1] = aa[1] - 80
+
+		total.append(alpha)
+
+	bb = np.asarray(total)
+	aa = bb.reshape(lenght, 8)
+	return aa
+
+
+
+def augmented_labels_90(label, n_grid_cells):
+	total_label = []
+
+	for i in range(n_grid_cells ** 2):
+
+		alpha = label[i]
+		aux = label[i]
+		if aux[0] == 1:
+			alpha[1] = 480 - aux[1]
+			alpha[2] = aux[2]
+			alpha[3] = aux[4]
+			alpha[4] = aux[3]
+		total_label.append(alpha)
+
+	d = np.asarray(total_label)
+	return d.reshape(100, 8)
+
+def augmented_labels_180(label, n_grid_cells):
+	total_label = []
+
+	for i in range(n_grid_cells ** 2):
+
+		alpha = label[i]
+		aux = label[i]
+		if aux[0] == 1:
+			alpha[1] = aux[1]
+			alpha[2] = 480 - aux[2]
+			alpha[3] = aux[4]
+			alpha[4] = aux[3]
+		total_label.append(alpha)
+	d = np.asarray(total_label)
+	return d.reshape(100, 8)
+
+def augmented_labels_270(label, n_grid_cells):
+	total_label = []
+
+	for i in range(n_grid_cells ** 2):
+
+		alpha = label[i]
+		aux = label[i]
+		if aux[0] == 1:
+			alpha[1] = 480 - aux[1]
+			alpha[2] = 480 - aux[2]
+			alpha[3] = aux[4]
+			alpha[4] = aux[3]
+		total_label.append(alpha)
+	d = np.asarray(total_label)
+	return d.reshape(100, 8)
+
+def invertir_labels_x(label, n_grid_cells):
+
+	total_label = []
+
+	for i in range(n_grid_cells ** 2):
+
+		alpha = label[i]
+		aux = label[i]
+		if aux[0] == 1:
+			alpha[1] = 480 - aux[1]
+		total_label.append(alpha)
+	d = np.asarray(total_label)
+	return d.reshape(100, 8)
+
+def invertir_labels_y(label, n_grid_cells):
+
+	total_label = []
+
+	for i in range(n_grid_cells ** 2):
+
+		alpha = label[i]
+		aux = label[i]
+		if aux[0] == 1:
+			alpha[2] = 480 - aux[2]
+		total_label.append(alpha)
+	d = np.asarray(total_label)
+	return d.reshape(100, 8)
+
+"""
+yy = give_label(tree)
+aa = labels_total(10, yy)
+bb = correct_label(aa)
+prueba = invertir_labels_x(bb, 10)
+"""
 
 def abrir_labels(n_imag):
 	path = "000"
 
-	total = []
+	labels = []
 
 	for i in range(n_imag):
 		path1 = int(path)
 		path1 += 1
 		path2 = str(path1)
 
-		tree = ET.parse("C:/Users/axel_/Desktop/Varios/BCCD_Dataset/BCCD/Annotations/BloodImage_00" + num_image + ".xml")
-
-		aa = np.asarray(give_label(tree))
-		bb = labels_total(aa)
-
-		total.append(bb)
+		tree = ET.parse("/home/oem/BCCD_Dataset/BCCD/Annotations/BloodImage_00" + path + ".xml")
 
 
-		total.append(bb)
-		total.append(bb)
-		total.append(bb)
-		total.append(bb)
-		total.append(bb)
-		total.append(bb)
-		total.append(bb)
-		total.append(bb)
-		total.append(bb)
+		yy = give_label(tree)
+		aa = labels_total(yy)
+		bb = correct_label(aa)
 
+		label_rotada1 = augmented_labels_90(bb, n_grid_cells=10)
+		label_rotada2 = augmented_labels_180(bb, n_grid_cells=10)
+		label_rotada3 = augmented_labels_270(bb, n_grid_cells=10)
 
+		h_l1 = invertir_labels_x(label_rotada1, n_grid_cells=10)
+		v_l1 = invertir_labels_y(label_rotada1, n_grid_cells=10)
 
+		h_l2 = invertir_labels_x(label_rotada2, n_grid_cells=10)
+		v_l2 = invertir_labels_y(label_rotada3, n_grid_cells=10)
+
+		h_l3 = invertir_labels_x(label_rotada2, n_grid_cells=10)
+		v_l3 = invertir_labels_y(label_rotada3, n_grid_cells=10)
+
+		labels.append(bb)
+
+		labels.append(label_rotada1)
+		labels.append(label_rotada2)
+		labels.append(label_rotada3)
+
+		labels.append(h_l1)
+		labels.append(v_l1)
+		labels.append(h_l2)
+		labels.append(v_l2)
+		labels.append(h_l3)
+		labels.append(v_l3)
 
 		path2 = path
 
-	return np.asarray(total)
+	return np.asarray(labels)
+
+
+alpha = abrir_labels(4)
+
+print(alpha.shape)
